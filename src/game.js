428e19912960d5fcff15e5b73b40d4d469293300
Cam Johnson <cameron.n.johnson15@gmail.com>
2023-11-30 17:13:01 -0700

    refactor: Refactor all functions to ES6

--- src/game.js
+++ src/game.js
@@ -5,22 +5,18 @@ const { createCard, evaluateGuess } = require('./card');
 const { createDeck, countCards } = require('./deck');
 const { createRound, takeTurn, calculatePercentCorrect, endRound } = require('./round');
 
-function printMessage(deck) {
+const printMessage = deck => 
   console.log(`Welcome to FlashCards! You are playing with ${countCards(deck)} cards.
   -----------------------------------------------------------------------`);
-}
 
-function printQuestion(round) {
-  util.main(round);
-}
+const printQuestion = round => util.main(round);
 
-function start() {
+const start = () => {
   const deck = createDeck(prototypeData);
   const round = createRound(deck);
   printMessage(deck);
   printQuestion(round);
   calculatePercentCorrect(round);
-  if (round.turns >= round.deck.length) console.log(endRound());
 }
 
 module.exports = {
