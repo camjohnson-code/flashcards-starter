feef490894cadbf53eeb936936d44ced2b575a56
Cam Johnson <cameron.n.johnson15@gmail.com>
2023-11-30 18:02:00 -0700

    test: Refactor tests using before()

--- test/deck-test.js
+++ test/deck-test.js
@@ -4,6 +4,43 @@ const expect = chai.expect;
 const { createCard, evaluateGuess } = require('../src/card');
 const { createDeck, countCards } = require('../src/deck');
 
+let deck1, deck2, deck3, numDeck1, numDeck2, numDeck3;
+
+before(function () {
+  const card1 = createCard(
+    1,
+    "What is Robbie's favorite animal",
+    ['sea otter', 'pug', 'capybara'],
+    'sea otter'
+  );
+  const card2 = createCard(
+    14,
+    'What organ is Khalid missing?',
+    ['spleen', 'appendix', 'gallbladder'],
+    'gallbladder'
+  );
+  const card3 = createCard(
+    12,
+    "What is Travis's middle name?",
+    ['Lex', 'William', 'Fitzgerald'],
+    'Fitzgerald'
+  );
+  const card4 = createCard(
+    12,
+    "What is the name of Cameron's dog?",
+    ['Lily', 'Bailey', 'Jojo'],
+    'Lily'
+  );
+
+  deck1 = createDeck([card1, card2]);
+  deck2 = createDeck([card1, card2, card3]);
+  deck3 = createDeck([card1, card2, card3, card4]);
+
+  numDeck1 = countCards(deck1);
+  numDeck2 = countCards(deck2);
+  numDeck3 = countCards(deck3);
+});
+
 describe('createDeck', function () {
   it('should be a function', function () {
     expect(createDeck).to.be.a('function');
@@ -11,66 +48,12 @@ describe('createDeck', function () {
 
   // Test that createDeck returns an object.
   it('should return an array of objects', function () {
-    const card1 = createCard(
-      1,
-      "What is Robbie's favorite animal",
-      ['sea otter', 'pug', 'capybara'],
-      'sea otter'
-    );
-    const card2 = createCard(
-      14,
-      'What organ is Khalid missing?',
-      ['spleen', 'appendix', 'gallbladder'],
-      'gallbladder'
-    );
-    const card3 = createCard(
-      12,
-      "What is Travis's middle name?",
-      ['Lex', 'William', 'Fitzgerald'],
-      'Fitzgerald'
-    );
-
-    const deck = createDeck([card1, card2, card3]);
-
-    expect(deck).to.be.an('object');
+    expect(deck3).to.be.an('object');
   });
 });
 
 describe('countCards', function () {
   it('should count the correct number of cards', function () {
-    const card1 = createCard(
-      1,
-      "What is Robbie's favorite animal",
-      ['sea otter', 'pug', 'capybara'],
-      'sea otter'
-    );
-    const card2 = createCard(
-      14,
-      'What organ is Khalid missing?',
-      ['spleen', 'appendix', 'gallbladder'],
-      'gallbladder'
-    );
-    const card3 = createCard(
-      12,
-      "What is Travis's middle name?",
-      ['Lex', 'William', 'Fitzgerald'],
-      'Fitzgerald'
-    );
-    const card4 = createCard(
-      12,
-      "What is the name of Cameron's dog?",
-      ['Lily', 'Bailey', 'Jojo'],
-      'Lily'
-    );
-
-    const deck1 = createDeck([card1, card2]);
-    const deck2 = createDeck([card1, card2, card3]);
-    const deck3 = createDeck([card1, card2, card3, card4]);
-
-    const numDeck1 = countCards(deck1);
-    const numDeck2 = countCards(deck2);
-    const numDeck3 = countCards(deck3);
-
     expect(numDeck1).to.equal(2);
     expect(numDeck2).to.equal(3);
     expect(numDeck3).to.equal(4);
